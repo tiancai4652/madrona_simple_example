22e35a87d7bc31b2ae753581570bf18ff84d3e0b
zhangran <zhangranmm@126.com>
2026-05-27 17:03:27 +0800

    1024-256d-sim

--- src/sim.hpp
+++ src/sim.hpp
@@ -38,14 +38,14 @@ constexpr int32_t MAX_TOPO_NODES = 10688;
 constexpr int32_t MAX_TOPO_LINKS = 41088;
 constexpr int32_t MAX_TOPO_PORTS = 41088;
 constexpr int32_t MAX_NODE_NEIGHBORS = 328;
-constexpr int32_t MAX_FLOWS = 677888;
+constexpr int32_t MAX_FLOWS = 2742272;
 constexpr int32_t MAX_PATH_NODES = 6;
 constexpr int32_t MAX_ECMP_NEXT_HOPS = 36;
 constexpr int32_t MAX_FLOW_ROUTE_STEPS = 6;
-constexpr int32_t MAX_DELAYED_EVENTS = 2908160;
-constexpr int32_t MAX_EVENTS_PER_STEP = 328704;
-constexpr int32_t MAX_TAG_INDEX = 2580480;
-constexpr int32_t MAX_SOURCE_TAGS = 645120;
+constexpr int32_t MAX_DELAYED_EVENTS = 12738560;
+constexpr int32_t MAX_EVENTS_PER_STEP = 2294784;
+constexpr int32_t MAX_TAG_INDEX = 10444800;
+constexpr int32_t MAX_SOURCE_TAGS = 2611200;
 // constexpr int32_t MAX_FLOW_COMPLETIONS = 68608;
 
 struct TopoNeighbor {
